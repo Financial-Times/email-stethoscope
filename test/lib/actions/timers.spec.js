9f72ed8d31efeec808aab50f1c6006b877cefccd
Melanie.Rogan <melanie.rogan@FT-MW5169.local>
2018-09-14 10:59:00 +0100

    updating package json and adding first tests

--- test/lib/actions/timers.spec.js
+++ test/lib/actions/timers.spec.js
@@ -1,1 +1,29 @@
 const { expect } = require('chai');
+const { startEvent } = require('../../../lib/actions/timers');
+console.log(startEvent);
+
+describe('timers', () => {
+    describe('startEvent', () => {
+        describe('with invalid args', () => {
+            it ('errors if event is missing', () => {
+               expect(() => startEvent({identifier:'PROCESSING_QUEUE'})).to.throw('event required')
+            });
+            it ('errors if identifier is missing', () => {
+                expect(() => startEvent({event:'PROCESSING_LIST'})).to.throw('identifier required')
+            });
+        });
+        describe('with valid args', () => {
+
+        });
+    });
+    describe('endEvent', () => {
+        describe('with invalid args', () => {
+
+        });
+        describe('with valid args', () => {
+
+        });
+
+    });
+});
+
