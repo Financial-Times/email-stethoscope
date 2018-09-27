399d9c458d1c48332851ba3feafababa923e1576
Abdullah Ali <abdullah-ali@hotmail.co.uk>
2018-09-27 16:06:55 +0100

    Add loggers.

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+	redisUrl: process.env.STETHOSCOPE_REDIS_URL,
+	keyTtl: 86400
+};
+
+if(process.env.NODE_ENV === 'test') {
+	config.redisUrl = 'redis://127.0.0.1:6379/0'
+}
+
+module.exports = config;
